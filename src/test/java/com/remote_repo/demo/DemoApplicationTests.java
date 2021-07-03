13c95cf2ba7f8e045eb84b8c8d0da62f5395a657
farzad <farzadnoruzi1900@gmail.com>
2021-07-03 15:10:01 +0200

    files

---
+++ src/test/java/com/remote_repo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remote_repo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
